2dbba317bd820557db4330fe26d3a437009e1042
PRASHANT PAL <118741353+prashant9569pal@users.noreply.github.com>
2023-04-29 23:13:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const hourel=document.getElementById("hour");
+const minutesel=document.getElementById("minutes");
+const secondsel=document.getElementById("seconds");
+const ampmel=document.getElementById("ampm");
+function updateClock()
+{
+    let h=new Date().getHours();
+    let m=new Date().getMinutes()
+    let s=new Date().getSeconds()
+    let ampm="AM";
+    if(h>12)
+    {
+        h=h-12;
+        ampm="PM";
+    }
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+    hourel.innerText=h;
+    minutesel.innerText=m;
+    secondsel.innerText=s;
+    ampmel.innerText=ampm;
+    setTimeout(()=>{updateClock()},1000);
+}
+updateClock();
